7f7fce6910dfd7b208102f11487820e328837ee0
fAndreuzzi <andreuzzi.francesco@gmail.com>
2024-08-08 11:16:02 +0200

    spotless and plugins

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,7 @@
 plugins {
     java
+    id("me.champeau.jmh") version "0.7.2"
+    id("com.diffplug.spotless") version "7.0.0.BETA1"
 }
 
 repositories {
@@ -7,6 +9,11 @@ repositories {
 }
 
 dependencies {
+    implementation("org.openjdk.jmh:jmh-core:1.37")
+    annotationProcessor("org.openjdk.jmh:jmh-generator-annprocess:1.37")
+    implementation("org.apache.commons:commons-lang3:3.15.0")
+    implementation("org.eclipse.collections:eclipse-collections:11.1.0")
+
     testImplementation(libs.junit.jupiter)
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
@@ -19,4 +26,13 @@ java {
 
 tasks.named<Test>("test") {
     useJUnitPlatform()
+}
+
+spotless {
+    java {
+        importOrder()
+        removeUnusedImports()
+        cleanthat()
+        formatAnnotations()
+    }
 }
