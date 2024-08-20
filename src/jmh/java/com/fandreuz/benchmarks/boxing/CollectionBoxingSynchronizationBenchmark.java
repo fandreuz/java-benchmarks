30e844b670027be329e9c95d1ddff90ce2c28811
fAndreuzzi <andreuzzi.francesco@gmail.com>
2024-08-20 13:38:51 +0200

    synch benchmark

---
+++ src/jmh/java/com/fandreuz/benchmarks/boxing/CollectionBoxingSynchronizationBenchmark.java
@@ -0,0 +1,51 @@
+package com.fandreuz.benchmarks.boxing;
+
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.TimeUnit;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+import org.eclipse.collections.api.factory.primitive.ObjectIntMaps;
+import org.eclipse.collections.api.map.primitive.MutableObjectIntMap;
+import org.eclipse.collections.impl.map.mutable.primitive.SynchronizedObjectIntMap;
+import org.openjdk.jmh.annotations.Benchmark;
+import org.openjdk.jmh.annotations.BenchmarkMode;
+import org.openjdk.jmh.annotations.Mode;
+import org.openjdk.jmh.annotations.OutputTimeUnit;
+import org.openjdk.jmh.annotations.Scope;
+import org.openjdk.jmh.annotations.State;
+
+public class CollectionBoxingSynchronizationBenchmark {
+
+    @State(Scope.Benchmark)
+    public static class BenchmarkState extends Utils.AbstractBenchmarkState {
+        public Map<String, Integer> map = Utils.STRINGS.stream() //
+                .collect(Collectors.toMap( //
+                        Function.identity(), //
+                        String::hashCode, //
+                        (integer, integer2) -> integer, //
+                        ConcurrentHashMap::new) //
+                );
+
+        public MutableObjectIntMap<String> primitiveMap = new SynchronizedObjectIntMap<>(ObjectIntMaps.mutable.empty());
+
+        public BenchmarkState() {
+            Utils.STRINGS.forEach(s -> primitiveMap.put(s, s.hashCode()));
+        }
+    }
+
+    @Benchmark
+    @BenchmarkMode(Mode.Throughput)
+    @OutputTimeUnit(TimeUnit.MILLISECONDS)
+    public int testStandardHashMap(BenchmarkState state) {
+        return state.map.get(Utils.STRINGS.get(state.getIndexAndIncrement()));
+    }
+
+    @Benchmark
+    @BenchmarkMode(Mode.Throughput)
+    @OutputTimeUnit(TimeUnit.MILLISECONDS)
+    public int testPrimitiveMap(BenchmarkState state) {
+        return state.primitiveMap.get(Utils.STRINGS.get(state.getIndexAndIncrement()));
+    }
+
+}
